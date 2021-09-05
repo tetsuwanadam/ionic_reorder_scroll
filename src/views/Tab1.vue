2ed40dd0b7faf9fa6ddc84fec02d3dd19f58a5b4
Adam C <cp@altpop.com>
2021-09-05 13:53:47 +0900

    Add reorder scroll test to Tab1

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,28 +1,58 @@
 <template>
   <ion-page>
-    <ion-header>
+    <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Reorder scroll test</ion-title>
       </ion-toolbar>
     </ion-header>
+    
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Reorder scroll test</ion-title>
         </ion-toolbar>
       </ion-header>
     
-      <ExploreContainer name="Tab 1 page" />
+      <ion-reorder-group @ionItemReorder="doReorder($event)" disabled="false">
+        <ion-item v-for="item of items" :key="item">
+          <ion-label>
+            Item {{ item }}
+          </ion-label>
+          <ion-reorder slot="end"></ion-reorder>
+        </ion-item>
+      </ion-reorder-group>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonItem, IonLabel, IonReorder, IonReorderGroup } from '@ionic/vue';
+import { defineComponent } from 'vue';
 
-export default  {
+export default defineComponent({
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonItem,
+    IonLabel,
+    IonReorder,
+    IonReorderGroup,
+  },
+  setup: function() {
+    return {
+      doReorder: function(event: CustomEvent) {
+        event.detail.complete();
+      },
+      items: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+    }
+  }
+});
 </script>
+
+<style scoped>
+</style>
